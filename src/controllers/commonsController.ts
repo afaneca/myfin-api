7fec83785df0ecc691aca9c30b62c00de725a4ac
António Faneca <me@afaneca.com>
2024-12-28 17:34:57 +0000

    [#15] Create flow for initial setup

--- src/controllers/commonsController.ts
+++ src/controllers/commonsController.ts
@@ -12,6 +12,9 @@ import { Request } from "express";
  */
 
 const checkAuthSessionValidity = async (req: Request, renewTrustLimit = true) => {
+  const userCount = await userService.getUserCount();
+  if(userCount == 0) throw APIError.notFound('No registered users were found.')
+
   const bypassSessionChecking = process.env.BYPASS_SESSION_CHECK === 'true';
   Logger.addLog(`bypass: ${process.env.BYPASS_SESSION_CHECK}`);
 
---
+++ src/services/setupService.ts
@@ -0,0 +1,20 @@
+import { performDatabaseRequest, prisma } from "../config/prisma.js";
+import userService from "./userService.js";
+import APIError from "../errorHandling/apiError.js";
+
+const initInstance = async(
+  username: string,
+  password: string,
+  email: string,
+  dbClient = undefined
+) => performDatabaseRequest(async (prismaTx) => {
+  const userCount = await userService.getUserCount();
+  // We should only allow access to this functionality if no users have been registered yet
+  if(userCount != 0) throw APIError.notAcceptable()
+
+  return userService.createUser({username, password, email}, prismaTx);
+}, dbClient);
+
+export default {
+  initInstance,
+}
