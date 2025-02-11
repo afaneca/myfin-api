685e7fd767aca5624fcd751fc5324e2b6eb12a6a
António Faneca <me@afaneca.com>
2025-02-11 17:43:31 +0000

    [#120] GET stats/dashboard/month-expenses-income-distribution: improve error handling

--- src/services/statsService.ts
+++ src/services/statsService.ts
@@ -37,7 +37,7 @@ const getExpensesIncomeDistributionForMonth = async (
       },
     })
     .catch(() => {
-      throw APIError.notFound();
+      return {};
     });
 
   const budgetId = budget.budget_id as bigint;
