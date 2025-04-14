b3a4bc5fe16ba2638ba7e265e762126b62c33cb1
António Faneca <me@afaneca.com>
2025-04-14 15:04:38 +0100

    [#141] added rationale field to apiError object

--- src/errorHandling/apiError.ts
+++ src/errorHandling/apiError.ts
@@ -1,35 +1,40 @@
 export default class APIError extends Error {
   code: number;
-
   message: string;
+  rationale: string;
 
-  constructor(code: number, message: string) {
+  constructor(code: number, message: string, rationale?: string) {
     super(message);
     this.code = code;
     this.message = message;
+    this.rationale = rationale;
   }
 
-  static badRequest(msg = "Unknown error") {
-    return new APIError(400, msg);
+  static badRequest(msg = "Unknown error", rationale?: string) {
+    return new APIError(400, msg, rationale);
   }
 
-  static notAuthorized(msg = "The owner of this request is not authorized to do so.") {
-    return new APIError(401, msg);
+  static notAuthorized(msg = "The owner of this request is not authorized to do so.", rationale?: string) {
+    return new APIError(401, msg, rationale);
   }
 
-  static forbidden(msg = "The owner of this request is not authorized to do so.") {
-    return new APIError(403, msg);
+  static forbidden(msg = "The owner of this request is not authorized to do so.", rationale?: string) {
+    return new APIError(403, msg, rationale);
   }
 
-  static notFound(msg = "The requested resource could not be found.") {
-    return new APIError(404, msg);
+  static notFound(msg = "The requested resource could not be found.", rationale?: string) {
+    return new APIError(404, msg, rationale);
   }
 
-  static notAcceptable(msg = "The request is not acceptable.") {
-    return new APIError(406, msg);
+  static notAcceptable(msg = "The request is not acceptable.", rationale?: string) {
+    return new APIError(406, msg, rationale);
   }
 
-  static internalServerError(msg = "Unknown error") {
-    return new APIError(500, msg);
+  static internalServerError(msg = "Unknown error", rationale?: string) {
+    return new APIError(500, msg, rationale);
   }
+}
+
+export enum CommonApiErrorCode {
+  RequestPayloadTooLarge = "REQUEST_PAYLOAD_TOO_LARGE",
 }
