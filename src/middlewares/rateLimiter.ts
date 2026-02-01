5e68cf1e1228de8fafa00cb389b4b6112f1fafff
António Faneca <me@afaneca.com>
2026-02-01 18:39:38 +0000

    [#197] Fix: increase rate limit to 100 requests per minute #197

--- src/middlewares/rateLimiter.ts
+++ src/middlewares/rateLimiter.ts
@@ -22,7 +22,7 @@ export const rateLimiter = (app?: Application): RequestHandler => {
 
   return rateLimit({
     windowMs: 60 * 1000,
-    limit: 10, // Max: 100 requests per minute
+    limit: 100, // Max: 100 requests per minute
     message: 'You have exceeded your 100 requests per minute limit.',
     legacyHeaders: true,
   });
